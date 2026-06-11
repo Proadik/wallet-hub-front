f30caef5c8074d046742786a5890e91d5572c5da
AdilK <proadik97@gmail.com>
2026-06-11 18:16:08 +0500

    version fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'wallet-hub': path.resolve(__dirname, '../wallet-hub/src'),
-    },
-  },
 })
